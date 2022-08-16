e19a0b8d14cd688f629c258134469ecbfa681b41
lenovo <lenovo@LAPTOP-K8650JEP>
2022-08-16 11:18:20 +0530

    first commit

---
+++ Acharya_day04/src/controlstatement/Forloop.java
@@ -0,0 +1,10 @@
+package controlstatement;
+
+public class Forloop {
+	public static void main(String[] args)
+	{
+	for(int i = 0; i < 3; i++) {
+	System.out.println(i);
+	}
+	}
+}
---
+++ Acharya_day04/src/controlstatement/Statementelse_if.java
@@ -0,0 +1,25 @@
+package controlstatement;
+
+public class Statementelse_if {
+	public static void main(String[] args) {
+
+		String lang = "Java";
+
+		if(lang == "Python") {
+
+		System.out.println("language is python");
+
+		}else if (lang == "C++") {
+
+		System.out.println("lang is C++");
+
+		}else if(lang == "Java") {
+
+		System.out.println("lang is Java");
+
+		}else {
+
+		System.out.println(lang); }
+
+		}
+}
